04e9952e8b3d1ceede2d78fe54c13e49688fa14b
Tony <anthonyalexanderburger@gmail.com>
2020-04-19 12:59:49 -0400

    Working on lesson five

--- DigipetCapstone/L1LessonFive.swift
+++ DigipetCapstone/L1LessonFive.swift
@@ -12,5 +12,57 @@ import UIKit
 import Foundation
 
 class L1LessonFive: SKScene {
+    var done: Bool!
+    var Pet1: SKSpriteNode!
+    var Pet2: SKSpriteNode!
+    var sBubble1: SKSpriteNode!
+    var sBubble2: SKSpriteNode!
+    var pet1Text: SKLabelNode!
+    var pet2Text: SKLabelNode!
+    
+    var headerText: SKLabelNode!
+    
+    var array: [() -> ()] = []
+    var correctAnswers: [[[String]]] = [[]]
+    var counter: Int = 0
+    
+    var lang: Int = 0
+    
+    var rotateAction: SKAction!
+    var pointAction: SKAction!
+    var walkInAction2: SKAction!
+    var walkIn2: SKAction!
+    var walkOut: SKAction!
+    
+    let petImgs = ["china", "spain", "france"]
+    
+    // pet's text to converse with user
+    var prompts: [[String]] = [[], [], []]
+    
+    override func didMove(to view: SKView) {
+        if let language = UserDefaults.standard.string(forKey: "language") {
+            switch language {
+            case "zh_Hans":
+                lang = 0
+                break
+            case "fr_FR":
+                lang = 2
+                break
+            case "es_ES":
+                lang = 1
+                break
+            default:
+                lang = 0
+                
+            }
+        } else {
+            lang = 0
+        }
+        done = false
+        
+        // todo base on lines 67-162 of L1LessonOne.swift
+        
+        
+    }
     
 }
