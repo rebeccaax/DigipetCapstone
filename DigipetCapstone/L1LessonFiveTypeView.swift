8e2757b564bf54dcd0ac054998c6fa75d66a716a
Tony <anthonyalexanderburger@gmail.com>
2020-04-23 10:51:29 -0400

    Created L1LessonFiveTypeView
    It has no content yet, but the transition to it doesn't crash the app

---
+++ DigipetCapstone/L1LessonFiveTypeView.swift
@@ -0,0 +1,16 @@
+//
+//  L1LessonFiveTypeView.swift
+//  DigipetCapstone
+//
+//  Created by Tony Burger on 4/23/20.
+//  Copyright © 2020 Katie Katz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class L1LessonFiveTypeView : UIViewController {
+    // todo
+}
