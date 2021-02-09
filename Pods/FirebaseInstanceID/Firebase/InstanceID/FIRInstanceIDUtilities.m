2f2023b4b214248cf3a8cf46d7881d5d32b47f0f
Tony <anthonyalexanderburger@gmail.com>
2021-02-09 11:37:58 -0500

    Updated pods

--- Pods/FirebaseInstanceID/Firebase/InstanceID/FIRInstanceIDUtilities.m
+++ Pods/FirebaseInstanceID/Firebase/InstanceID/FIRInstanceIDUtilities.m
@@ -14,22 +14,18 @@
  * limitations under the License.
  */
 
-#import "FIRInstanceIDUtilities.h"
+#import "Firebase/InstanceID/FIRInstanceIDUtilities.h"
 
 #if TARGET_OS_IOS || TARGET_OS_TV
 #import <UIKit/UIKit.h>
 #endif
 #import <sys/utsname.h>
 
-#import <FirebaseCore/FIROptions.h>
 #import <GoogleUtilities/GULUserDefaults.h>
-#import "FIRInstanceID.h"
-#import "FIRInstanceIDConstants.h"
-#import "FIRInstanceIDLogger.h"
-
-// Convert the macro to a string
-#define STR_EXPAND(x) #x
-#define STR(x) STR_EXPAND(x)
+#import "Firebase/InstanceID/FIRInstanceIDConstants.h"
+#import "Firebase/InstanceID/FIRInstanceIDLogger.h"
+#import "Firebase/InstanceID/Public/FIRInstanceID.h"
+#import "FirebaseCore/Sources/Private/FirebaseCoreInternal.h"
 
 static NSString *const kFIRInstanceIDAPNSSandboxPrefix = @"s_";
 static NSString *const kFIRInstanceIDAPNSProdPrefix = @"p_";
@@ -78,16 +74,29 @@ NSString *FIRInstanceIDCurrentAppVersion() {
   return version;
 }
 
+NSString *FIRInstanceIDBundleIDByRemovingLastPartFrom(NSString *bundleID) {
+  NSString *bundleIDComponentsSeparator = @".";
+
+  NSMutableArray<NSString *> *bundleIDComponents =
+      [[bundleID componentsSeparatedByString:bundleIDComponentsSeparator] mutableCopy];
+  [bundleIDComponents removeLastObject];
+
+  return [bundleIDComponents componentsJoinedByString:bundleIDComponentsSeparator];
+}
+
 NSString *FIRInstanceIDAppIdentifier() {
-  NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
-  if (!bundleIdentifier.length) {
+  NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
+  if (!bundleID.length) {
     FIRInstanceIDLoggerError(kFIRInstanceIDMessageCodeUtilitiesMissingBundleIdentifier,
                              @"The mainBundle's bundleIdentifier returned '%@'. Bundle identifier "
                              @"expected to be non-empty.",
-                             bundleIdentifier);
+                             bundleID);
     return @"";
   }
-  return bundleIdentifier;
+#if TARGET_OS_WATCH
+  return FIRInstanceIDBundleIDByRemovingLastPartFrom(bundleID);
+#endif
+  return bundleID;
 }
 
 NSString *FIRInstanceIDFirebaseAppID() {
@@ -111,7 +120,7 @@ NSString *FIRInstanceIDDeviceModel() {
 NSString *FIRInstanceIDOperatingSystemVersion() {
 #if TARGET_OS_IOS || TARGET_OS_TV
   return [UIDevice currentDevice].systemVersion;
-#elif TARGET_OS_OSX
+#elif TARGET_OS_OSX || TARGET_OS_WATCH
   return [NSProcessInfo processInfo].operatingSystemVersionString;
 #endif
 }
@@ -131,8 +140,11 @@ BOOL FIRInstanceIDHasLocaleChanged() {
 #pragma mark - Helpers
 
 BOOL FIRInstanceIDIsValidGCMScope(NSString *scope) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
   return [scope compare:kFIRInstanceIDScopeFirebaseMessaging
                 options:NSCaseInsensitiveSearch] == NSOrderedSame;
+#pragma clang diagnostic pop
 }
 
 NSString *FIRInstanceIDStringForAPNSDeviceToken(NSData *deviceToken) {
